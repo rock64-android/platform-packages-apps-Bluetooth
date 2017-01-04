6d529e749f2b7d4095cdb7e89b85afacbba39e55
hwg <hwg@rock-chips.com>
2017-01-04 11:10:57 +0800

    add realtek bt service, default is disabled
    Change-Id: I6407807a7a4f634f80c694b3f4c4c527e57a0e80

--- jni/com_android_bluetooth_btservice_AdapterService.cpp
+++ jni/com_android_bluetooth_btservice_AdapterService.cpp
@@ -1398,5 +1398,11 @@ jint JNI_OnLoad(JavaVM *jvm, void *reserved)
         return JNI_ERR;
     }
 
+#ifdef BLUETOOTH_RTK_API
+    if ((status = android::register_com_android_bluetooth_rtkbt(e)) < 0) {
+        ALOGE("jni rtkbt registration failure: %d", status);
+        return JNI_ERR;
+    }
+#endif
     return JNI_VERSION_1_6;
 }
--- src/com/android/bluetooth/btservice/Config.java
+++ src/com/android/bluetooth/btservice/Config.java
@@ -41,6 +41,7 @@ import com.android.bluetooth.gatt.GattService;
 import com.android.bluetooth.map.BluetoothMapService;
 import com.android.bluetooth.sap.SapService;
 import com.android.bluetooth.pbapclient.PbapClientService;
+import com.android.bluetooth.rtkbt.RtkbtService;
 
 public class Config {
     private static final String TAG = "AdapterServiceConfig";
@@ -51,6 +52,7 @@ public class Config {
     //Do not inclue OPP and PBAP, because their services
     //are not managed by AdapterService
     private static final Class[] PROFILE_SERVICES = {
+        RtkbtService.class,
         HeadsetService.class,
         A2dpService.class,
         A2dpSinkService.class,
@@ -68,6 +70,7 @@ public class Config {
      * Resource flag to indicate whether profile is supported or not.
      */
     private static final int[]  PROFILE_SERVICES_FLAG = {
+        R.bool.profile_supported_rtkbt,
         R.bool.profile_supported_hs_hfp,
         R.bool.profile_supported_a2dp,
         R.bool.profile_supported_a2dp_sink,
@@ -137,6 +140,8 @@ public class Config {
             profileIndex = BluetoothProfile.SAP;
         } else if (profile == PbapClientService.class) {
             profileIndex = BluetoothProfile.PBAP_CLIENT;
+        }else if (profile == RtkbtService.class) {
+            return false;
         }
 
         if (profileIndex == -1) {
